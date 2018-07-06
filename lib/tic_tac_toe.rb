d0177910bd6c7c9299f3d063412ff5368bdfbce8
Jonathan Hong <jonhong777@gmail.com>
2018-07-06 22:19:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -82,8 +82,6 @@ end
 # board = ["O", " ", "X", "O", "O", "X", "O", "X" , "X" ]
 def won?(board)
   WIN_COMBINATIONS.detect do |combo|
-    # combo = [3, 4, 5]
-    
     first_index = combo[0]     #first_index = 3 
     second_index = combo[1]    #second_index = 4
     third_index = combo[2]     #third_index = 5
