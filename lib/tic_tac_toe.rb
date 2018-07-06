9e408f3ee9483f370b4fea4e2f1d0ce6c6c2f721
Jonathan Hong <jonhong777@gmail.com>
2018-07-06 22:21:43 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -120,13 +120,7 @@ def over?(board)
 end
 
 def winner(board)
-  winning_combo = won?(board)
-  if winning_combo.class != Array
-    nil
-  else
-    winning_index = winning_combo[0]
-    winning_token = board[winning_index]
-  end
+  won?(board) # [3,4,5]
 end
 
 def play(board)
